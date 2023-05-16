e8663b3e8c2d1f2a8fcba066aa4b84c83a5a74aa
ArunGProjects <anuradhaey91@gmail.com>
2023-05-16 20:29:39 +0530

    commit

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -67,7 +67,7 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField
+        <FormField 
           labelName="Search images"
           type="text"
           name="text"
